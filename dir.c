cab1d5dfa6936a28f0712bd48bbf4f894fb19e55
Hilary Zen <hzen00@stuy.edu>
2019-11-19 02:17:58 -0500

    Added

--- dir.c
+++ dir.c
@@ -2,40 +2,50 @@
 #include <stdlib.h>
 #include <string.h>
 #include <dirent.h>
+#include <errno.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 
-int main() {
+int main(int argc, char *argv[]) {
   DIR *d;
-  char *path = "../structs/";
+  char path[100];
+  if (argc > 1) {
+    strcpy(path, argv[1]);
+  } else {
+    printf("Enter a directory to scan: ");
+    fgets(path, 100, stdin);
+    if (strchr(path, '\n') != NULL) {
+      *(strchr(path, '\n')) = '\0';
+    }
+  }
   char file_path[200];
   struct stat s;
   d = opendir(path);
   if (d == NULL) {
-    printf("Opening directory didn't work\n");
-  }
-  printf("Statistics for directory: %s\n", path);
+    printf("Opening directory didn't work: %s\n", strerror(errno));
+  } else {
+    printf("Statistics for directory: %s\n", path);
 
-  int dir_size = 0;
-  struct dirent *file;
-  file = readdir(d);
-  while (file != NULL) {
-    if (file->d_type == 4) {
-      printf("Directory: %s\n", file->d_name);
-    } else {
-      printf("File: %s\n", file->d_name);
-    }
-    sprintf(file_path, "%s%s", path, file->d_name);
-    stat(file_path, &s);
-    //printf("%d\n", s.st_size);
-    dir_size += s.st_size;
-    //printf("%s\n", file->d_name);
-    //printf("%d\n", file->d_type);
+    int dir_size = 0;
+    struct dirent *file;
     file = readdir(d);
+    while (file != NULL) {
+      if (file->d_type == 4) {
+        printf("Directory: %s\n", file->d_name);
+      } else {
+        printf("File: %s\n", file->d_name);
+      }
+      sprintf(file_path, "%s/%s", path, file->d_name);
+      stat(file_path, &s);
+      //printf("%d\n", s.st_size);
+      dir_size += s.st_size;
+      //printf("%s\n", file->d_name);
+      //printf("%d\n", file->d_type);
+      file = readdir(d);
+    }
+    closedir(d);
+
+    printf("Total Directory Size: %d\n", dir_size);
   }
-  closedir(d);
-
-  printf("Total Directory Size: %d\n", dir_size);
-
   return 0;
 }
